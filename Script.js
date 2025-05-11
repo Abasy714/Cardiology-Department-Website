dabd50de41d7ea8ed3d163cf80cc059973a0831c
EbrahimM0hamed <ebrahimmohamed7550@gmail.com>
2025-05-11 15:23:57 +0300

    about, signin

--- Script.js
+++ Script.js
@@ -1,1 +1,9 @@
 // Our Java Script code and any backend will be here
+window.addEventListener('scroll', () => {
+    const headerName = document.querySelector('.name');
+    if (window.scrollY > 50) { // Change styles after 50px scroll
+        headerName.classList.add('name-scroll');
+    } else {
+        headerName.classList.remove('name-scroll');
+    }
+});
